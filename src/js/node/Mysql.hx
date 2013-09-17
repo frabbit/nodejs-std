5a5ab123e72ebe8cdc73d4ba31056b6a7e85cc92
frabbit <frabbit77@googlemail.com>
2013-09-17 11:05:36 +0200

    minor

--- src/js/node/Mysql.hx
+++ src/js/node/Mysql.hx
@@ -1,6 +1,8 @@
 
 package js.node;
 
+import haxe.Constraints.Function;
+
 
 
 
@@ -9,31 +11,88 @@ package js.node;
  */
 
 typedef QueryError = Dynamic
-typedef QueryRows = Array<Dynamic>
+typedef QueryRow = Dynamic
+typedef QueryRows = Array<QueryRow>
 typedef QueryFields = Dynamic
 
-typedef QueryCallback = QueryError -> QueryRows -> ?QueryFields -> Void
+typedef MysqlErr = Dynamic;
 
-typedef MysqlConnection = {
-	public function connect ():Void;
-	public function query (q:String, cb:QueryCallback):Void;
-	public function end ():Void;
+typedef ConnectionError = Dynamic;
+
+typedef QueryCallback = QueryError -> QueryRows -> QueryFields -> Void
+
+abstract EscapedValue(Dynamic) {
+	inline function new (d:Dynamic) this = d;
+
+	@:to public inline function toString ():String return this;
 }
 
-typedef MysqlOptions = {
+extern class MysqlConnection 
+{
+	public function connect ():Void;
+	@:overload(function (q:String, escaped:Array<Dynamic>, cb:QueryCallback):Void {})
+	public function query (q:String, cb:QueryCallback):Void;
+
+	public function end (cb:ConnectionError->Void):Void;
+	public function destroy ():Void;
+
+	public function release ():Void;
+
+	public function pause ():Void;
+	public function resume ():Void;
+		
+	public function escape (v:Dynamic):EscapedValue;
+	public function escapeId (v:Dynamic):EscapedValue;
+
+	function on (event:String, cb:Function):MysqlConnection;
+
+	public inline function onError(f:MysqlErr->Void):MysqlConnection {
+		return on("error", f);
+	}
+
+	public inline function onFields(f:QueryFields->Void):MysqlConnection {
+		return on("fields", f);
+	}
+	public inline function onResult(f:QueryRow->Void):MysqlConnection {
+		return on("result", f);
+	}	
+
+}
+
+typedef MysqlConnectionOptions = {
 	host : String,
 	user : String,
 	password : String,
 	?port : Int,
-	?database : String
+	?database : String,
+	?multipleStatements: Bool
 	// more options see https://github.com/frabbit/node-mysql/tree/kp
+}
+
+typedef PoolErr = Dynamic;
+
+extern class MysqlPool {
+	public function getConnection(f:PoolErr->MysqlConnection->Void):Void;
+
+	public inline function onConnection(f:PoolErr->MysqlConnection->Void):Void {
+		(this:Dynamic).on("connection", f);
+	}
+}
+
+typedef MysqlPoolOptions = { > MysqlConnectionOptions,
+	?createConnection : MysqlConnectionOptions -> MysqlConnection,
+	?waitForConnections : Bool,
+	?connectionLimit : Int,
+	?queueLimit : Int
 }
 
 @:native("js_node_Mysql")
 extern 
 class Mysql {
 
-	public static function createConnection (options:MysqlOptions):MysqlConnection;
+	// TODO createPoolCluster
+	public static function createConnection (options:MysqlConnectionOptions):MysqlConnection;
+	public static function createPool(options:MysqlPoolOptions):MysqlPool;
 
 	private static inline function __init__(): Void untyped {
 
